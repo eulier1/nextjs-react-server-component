39172c6ab7512f1b9d094a499b8953f4b36526c7
Eulier González <eulier1@gmail.com>
2025-01-07 17:27:52 -0300

    server action mutation

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -5,7 +5,7 @@ console.log(process.argv);
 
 const getData = async () => {
   // To simulate error, uncommented this line to see the normal page
-  await new Promise((resolve, reject) => setTimeout(() => reject(), 2000));
+  // await new Promise((resolve, reject) => setTimeout(() => reject(), 2000));
 
   const todos = await db.todo.findMany({});
   return todos;
--- utils/actions.ts
+++ utils/actions.ts
@@ -2,6 +2,19 @@
 import { revalidatePath } from 'next/cache';
 import db from './db';
 
+export const completeTodo = async (id) => {
+  await db.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      completed: true,
+    },
+  });
+
+  revalidatePath('/todos');
+};
+
 export const newTodo = async (formData: FormData) => {
   const content = formData.get('content')?.toString() ?? '';
 
